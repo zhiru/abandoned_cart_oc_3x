7e8aa7cb65e0882ff56ff025e274c3f46795fcd6
Felipe <felipe@aireset.com.br>
2022-11-06 17:01:56 -0300

    ajustes para pt-br

---
+++ upload/admin/language/pt-br/extension/module/abandoned_carts.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']          = 'Carrinhos abandonados';
+
+// Text
+$_['text_bailed_carts']      = 'Carrinhos abandonados';
+$_['text_edit']              = 'Editar opções de carrinho abandonado';
+$_['text_extension']         = 'Extensões';
+$_['text_list']              = 'Visão geral do carrinho de socorro';
+$_['text_no_results']        = 'Boas notícias!Sem carrinhos abandonados recentes.';
+$_['text_success']           = 'A configuração de carrinhos abandonada foi salva!';
+
+// Email Body
+$_['subject_prefix']         = "Ordem incompleta em";
+$_['failed_cart_greeting']   = 'Oi, %s';
+$_['failed_cart_intro']      = 'Percebemos que você parou em nossa loja recentemente e não completou sua compra;Nós só queríamos garantir que isso não estivesse por erro - se você pretendia concluir a finalização da compra, você não concluiu o processo de pagamento e, portanto, seu pedido não foi colocado.';
+$_['failed_cart_contents']   = 'Para sua conveniência, aqui está uma lista do que estava no seu carrinho:';
+$_['failed_cart_body']       = 'Se o seu carrinho de socorro foi intencional, adoramos ouvir qualquer feedback, sugestões ou reclamações que você possa oferecer para que possamos melhorar nossa experiência de loja e cliente!';
+$_['failed_cart_footer']     = 'Você recebeu esta mensagem porque você (ou alguém usando seu endereço de e -mail) recentemente interrompeu em nossa loja - você não foi adicionado a nenhum boletim informativo e não receberá mais comunicação de nós como resultado de sua tentativa de pedido (a menos que você se inscreveu explicitamente aténossa newsletter e/ou notificações de produto).Não assinamos com força as pessoas com nossas comunicações.';
+$_['failed_cart_signoff']    = 'Agradecemos por seu interesse em nosso mercadoria;tenha um excelente dia!';
+$_['failed_cart_signature']  = 'Cumprimentos,';
+
+// Entry
+$_['entry_abandoned_status'] = 'Critério';
+$_['help_abandoned_status']  = 'Os status (s) da ordem para considerar abandonados';
+$_['entry_limit']            = 'Dia(s) to search for bailed carts';
+$_['entry_status']           = 'status';
+
+// Button
+$_['button_view']            = 'Ver detalhes da ordem abandonada';
---
+++ upload/admin/language/pt-br/extension/module/reports/abandoned_carts.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']        = 'Carrinhos abandonados';
+$_['button_recover']       = 'Envie uma mensagem para carrinhos de socorro selecionados';
+
+// Text
+$_['text_list']            = 'Visão geral do carrinho de socorro';
+$_['text_missing']         = 'Pedidos ausentes';
+$_['text_search']          = 'Se nenhuma transação aparecer na pesquisa do cliente, os registros contados são carrinhos de socorro';
+$_['text_success']         = 'Um e -mail foi enviado para carrinhos selecionados!';
+$_['text_deleted']         = 'Transações selecionadas foram excluídas';
+
+// Column
+$_['column_order_id']      = 'ID do pedido';
+$_['column_customer']      = 'Cliente';
+$_['column_status']        = 'status';
+$_['column_date_added']    = 'data adicionada';
+$_['column_date_modified'] = 'Data modificada';
+$_['column_abandoned'] 	   = 'Contatada?';
+$_['column_total']         = 'total';
+$_['column_action']        = 'Ação';
+
+// Error
+$_['warning_duplicate']    = 'Existem %d pedidos no sistema com IP de %s.Certifique -se de não enviar mensagens duplicadas ou verifique a fila pendente para novos pedidos.';
+$_['error_permission']     = 'Você não tem permissão para modificar carrinhos abandonados.';
